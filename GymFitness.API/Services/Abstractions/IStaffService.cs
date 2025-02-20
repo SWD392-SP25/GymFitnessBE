57724969bd7c0864e4b0ba5fd0bc0e8847807e42
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-02-20 22:54:10 +0700

    Google-Login-Complete

---
+++ GymFitness.API/Services/Abstractions/IStaffService.cs
@@ -0,0 +1,10 @@
+﻿using GymFitness.Infrastructure.Data;
+
+namespace GymFitness.API.Services.Abstractions
+{
+    public interface IStaffService
+    {
+        Task<Staff> GetStaffByEmail(string email);
+        void AddStaff(Staff staff);
+    }
+}
---
+++ GymFitness.Infrastructure/Repositories/Abstractions/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using GymFitness.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymFitness.Infrastructure.Repositories.Abstractions
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByEmail(string email);
+        void AddUser(User user);
+
+    }
+}
