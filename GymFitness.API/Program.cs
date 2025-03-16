6d938e7bbad51a09b9dce4d6be91e90393942023
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-03-16 23:53:18 +0700

    Connection String

--- GymFitness.API/Program.cs
+++ GymFitness.API/Program.cs
@@ -107,12 +107,12 @@ namespace GymFitness.API
 
 
             // ✅ Thêm DbContext
-            //builder.Services.AddDbContext<GymbotDbContext>(options =>
-            //   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<GymbotDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             ////Azure Db
-            builder.Services.AddDbContext<GymbotDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
+            //builder.Services.AddDbContext<GymbotDbContext>(options =>
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
 
 
 
