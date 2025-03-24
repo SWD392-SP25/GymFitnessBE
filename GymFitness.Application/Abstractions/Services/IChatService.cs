7e7e492f93c07478c16c0d5bc09d0e1b1542aeb9
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-03-24 16:22:58 +0700

    Duplicate

---
+++ GymFitness.Application/Abstractions/Services/IChatService.cs
@@ -0,0 +1,15 @@
+﻿using GymFitness.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymFitness.Application.Abstractions.Services
+{
+    public interface IChatService
+    {
+        Task<IEnumerable<ChatHistory>> GetChatHistory(Guid senderId, Guid receiverId);
+        Task<ChatHistory> SendMessage(int senderId, int receiverId, string messageText, bool isUserMessage);
+    }
+}
