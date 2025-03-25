99aef04aad3087bd4fe5ef54627d111d1a30ff5a
Loc Dang <locdpxse182614@fpt.edu.vn>
2025-03-25 09:05:23 +0700

    Chat Hub V1

--- GymFitness.Application/Abstractions/Services/IChatService.cs
+++ GymFitness.Application/Abstractions/Services/IChatService.cs
@@ -9,7 +9,7 @@ namespace GymFitness.Application.Abstractions.Services
 {
     public interface IChatService
     {
-        Task<IEnumerable<ChatHistory>> GetChatHistory(Guid senderId, Guid receiverId);
-        Task<ChatHistory> SendMessage(int senderId, int receiverId, string messageText, bool isUserMessage);
+        Task AddMessageAsync(ChatHistory chatMessage);
+        Task<List<ChatHistory>> GetChatHistoryAsync(Guid userId, Guid staffId);
     }
 }
